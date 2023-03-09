57baab3ec42cfed6cb9c57c44f1c4462bde35369
jhonatanjosue11@outlook.es <jhonatanjosue11@outlook.es>
2023-03-08 23:35:45 -0400

    Actualizacion 1

--- Project1/principal.cpp
+++ Project1/principal.cpp
@@ -13,4 +13,30 @@ int main() {
 	escenarioInicial.setNombre("Inicia la historia Tetris");
 	//Implementar la parte de 
 	cout << "El nombre del escenario es:" << escenarioInicial.getNombre() << endl;
+
+	int Tablero;
+	{
+		cout << "Espacio donde se almacenan las figuras" << endl;
+	}
+	
+	
+	int Bloques;
+	{
+		cout << "Son los cuales depende todo el juego y dando asi la vida al mismo " << endl;
+	}
+	
+	int Puntuacion;
+	{
+		cout << "Lugar donde se guardan todos los puntos que gana el usuario" << endl;
+	}
+
+	int Escenario;
+	{
+		cout << "Espacio que contrendra bloques que esten por ingresar,puntuacion,nivel,opciones" << endl;
+	}
+
+
+	 
+	
+
 }
